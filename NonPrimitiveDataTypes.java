32abc9ea358f75a0a5677bef8d0cb40ee9642a18
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-03 15:37:15 +0530

    Add Non Primitive Data Types example

---
+++ NonPrimitiveDataTypes.java
@@ -0,0 +1,12 @@
+public class NonPrimitiveDataTypes {
+    public static void main(String[] args) {
+        // String is a non-primitive data type
+        String name = "Ayush";
+
+        // Array stores multiple values
+        int[] numbers = {10, 20, 30};
+
+        System.out.println("Name: " + name);
+        System.out.println("First number: " + numbers[0]);
+    }
+}
