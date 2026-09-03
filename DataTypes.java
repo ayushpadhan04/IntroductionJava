0b320d74e2d633b6996da4371061d95b88df8bae
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-03 15:37:08 +0530

    Add Data Types example

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Different primitive data types in Java
+        int number = 10;
+        double price = 99.50;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Number: " + number);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
