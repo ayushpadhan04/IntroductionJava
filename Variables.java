435608837719613558c021497ffe05b41ef22363
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-03 15:37:04 +0530

    Add Variables example

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        // A variable stores a value
+        String name = "Ayush";
+        int age = 20;
+        double height = 6.0;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+}
