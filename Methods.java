bd8d7e961ae3432e1fdddc692de16a66153feab7
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-03 15:37:58 +0530

    Add Methods example

---
+++ Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+
+    // This method prints a message
+    static void sayHello() {
+        System.out.println("Hello, Ayush!");
+    }
+
+    public static void main(String[] args) {
+        // Calling the method
+        sayHello();
+    }
+}
