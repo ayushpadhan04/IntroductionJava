69669f2aad10659121a9896d436f781ebf2f2667
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-03 15:37:39 +0530

    Add For Loop example

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // For loop repeats a block of code
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
