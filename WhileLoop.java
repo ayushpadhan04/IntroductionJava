d243321c551a44d97d61ba62a45c519538ff42bd
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-03 15:37:44 +0530

    Add While Loop example

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        // While loop runs while the condition is true
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            i++;
+        }
+    }
+}
