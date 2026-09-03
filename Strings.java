ea5b457cbb1696329457e1fccebfcff1e46f5da2
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-03 15:37:54 +0530

    Add Strings example

---
+++ Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String[] args) {
+        // String is used to store text
+        String name = "Ayush Padhan";
+
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+    }
+}
