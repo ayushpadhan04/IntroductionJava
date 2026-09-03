6a182fea3f69d303534ce873426af174121f54d1
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-03 15:37:49 +0530

    Add Arrays example

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // An array stores multiple values of the same type
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("First number: " + numbers[0]);
+        System.out.println("Second number: " + numbers[1]);
+    }
+}
